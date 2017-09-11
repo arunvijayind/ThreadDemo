eb6e930fa7cfcdea5d8f432169374fd36527df93
arunvijayind <arunvijayind@gmail.com>
2017-09-11 16:03:31 +0530

    Create WithdrawThread.java

---
+++ bankingSystem/WithdrawThread.java
@@ -0,0 +1,17 @@
+package bankingSystem;
+
+public class WithdrawThread implements Runnable {
+	
+	private Account account;
+	private double amount;
+	
+	public WithdrawThread(Account account, double amount) {
+		this.account = account;
+		this.amount = amount;
+	}
+	
+	public void run() {
+		//make a withdraw
+		account.withdraw(amount);
+	}
+}
