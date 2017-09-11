dbbe850b9674cf1e5b94aac5c8e9012f828c4515
arunvijayind <arunvijayind@gmail.com>
2017-09-11 16:05:14 +0530

    Update Account.java

--- bankingSystem/Account.java
+++ bankingSystem/Account.java
@@ -1,11 +1,13 @@
 package bankingSystem;
 
 public class Account {
+	
 	private double balance = 0;
+	
 	public Account(double balance) {
 		this.balance = balance;
 	}
-	// if ‘synchronized’ is removed, the outcome is unpredictable
+	
 	public synchronized void deposit(double amount) {
 		if (amount < 0) {
 			throw new IllegalArgumentException("Can't deposit.");
@@ -15,7 +17,7 @@ public class Account {
 				+Thread.currentThread().getId()
 				+", balance is " +balance);
 	}
-	// if ‘synchronized’ is removed, the outcome is unpredictable
+	
 	public synchronized void withdraw(double amount) {
 		if (amount < 0 || amount > this.balance) {
 			throw new IllegalArgumentException("Can't withdraw.");
